50f67b0d0ee0342239cde9094f55574a85dff16e
uucyan <uuunouucyan@gmail.com>
2021-09-03 23:43:14 +0900

    chore: ESLint の設定を自分好みに修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,27 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    indent: ['error', 2],
+    "vue/html-indent": ["error", 2],
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/require-prop-types': 'off',
+    'no-undef': 'off',
   },
 };
